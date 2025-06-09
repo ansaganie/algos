c7b9914fce2e96a14905e91d41b5a5b967b44aa6
Ansagan Islamgali <ansaganie@hotmail.com>
2025-06-09 17:42:29 +0500

    refactor heightChecker function to simplify logic and improve readability

--- leetcode/1051. Height Checker.js
+++ leetcode/1051. Height Checker.js
@@ -2,23 +2,36 @@
  * @param {number[]} heights
  * @return {number}
  */
+// var heightChecker = function(heights) {
+//     var arr = new Array(101).fill(0);
+
+//     for (var i = 0; i < heights.length; i++) {
+//         arr[heights[i]]++;
+//     }
+
+//     var c = 0
+//     var j = 0;
+
+//     for (var i = 1; i < arr.length; i++) {
+//         while (arr[i]-- > 0) {
+//             if (heights[j] !== i) {
+//                 c++;
+//             }
+//             j++;
+//         }
+//     }
+
+//     return c;
+// };
+
+
 var heightChecker = function(heights) {
-    var arr = new Array(101).fill(0);
+    var cp = heights.concat([]);
+    cp.sort((a, b) => a - b);
+    var c = 0;
 
     for (var i = 0; i < heights.length; i++) {
-        arr[heights[i]]++;
-    }
-
-    var c = 0
-    var j = 0;
-
-    for (var i = 1; i < arr.length; i++) {
-        while (arr[i]-- > 0) {
-            if (heights[j] !== i) {
-                c++;
-            }
-            j++;
-        }
+        if (heights[i] !== cp[i]) c++;
     }
 
     return c;
