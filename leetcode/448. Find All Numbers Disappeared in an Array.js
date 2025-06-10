de0d3302f2edeb04ae3b90f4d6844168ce448d75
Ansagan Islamgali <ansaganie@hotmail.com>
2025-06-10 14:29:58 +0500

    refactor findDisappearedNumbers function to remove unused code and improve clarity

--- leetcode/448. Find All Numbers Disappeared in an Array.js
+++ leetcode/448. Find All Numbers Disappeared in an Array.js
@@ -2,12 +2,36 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findDisappearedNumbers = function(nums) {
-    var arr = new Array(nums.length).fill(0);
+
+// var findDisappearedNumbers = function(nums) {
+//     var arr = new Array(nums.length).fill(0);
     
-    for (var n of nums) {
-        arr[n - 1] = 1;
+//     for (var n of nums) {
+//         arr[n - 1] = 1;
+//     }
+    
+//     return arr.reduce((r, a, i) => { if (a !== 1) r.push(i + 1); return r; } ,[]);
+// };
+
+var findDisappearedNumbers = function (nums) {
+    for (var i = 0; i < nums.length; i++) {
+        var n = nums[i];
+        if (n < 0) continue;
+        while (n > 0) {
+            var next = nums[n - 1];
+            nums[n - 1] = -n;
+            n = next;
+        }
     }
-    
-    return arr.reduce((r, a, i) => { if (a !== 1) r.push(i + 1); return r; } ,[]);
+
+    var c = 0;
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) {
+            nums[c++] = i + 1;
+        }
+    }
+
+    nums.length = c;
+
+    return nums;
 };
