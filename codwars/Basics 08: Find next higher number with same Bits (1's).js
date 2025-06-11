d83d855b2c6b81466f9b59d75835054f27c4aff0
Ansagan Islamgali <ansaganie@hotmail.com>
2025-06-11 17:46:56 +0500

    refactor nextHigher function to improve logic and readability

--- codwars/Basics 08: Find next higher number with same Bits (1's).js
+++ codwars/Basics 08: Find next higher number with same Bits (1's).js
@@ -1,6 +1,15 @@
 function nextHigher(n) {
-    
+    let bits = n.toString(2);
+    let ones = bits.split('1').length - 1;
+    let next = n + 1;
 
+    while (true) {
+        let nextBits = next.toString(2);
+        if (nextBits.split('1').length - 1 === ones) {
+            return next;
+        }
+        next++;
+    }
 }
 
 console.log(nextHigher(128)) // 256
